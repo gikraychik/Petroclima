e60895e69b0f4ba7ab9dea1a71087f12bbf9be2f
George Kraychik <gkraychik@localhost.localdomain>
2014-01-22 17:33:06 +0400

    Products changed: punkt 12 added. New TU added.

---
+++ petroclimaru/www/ukpv02a.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <title>УК ПВ исп.02А</title>
+<script language="JavaScript" src="Base/JavaScripts/script.js">
+</script>
+  <meta http-equiv="Content-Type"
+ content="text/html; charset=utf-8">
+  <link
+ href="Base/Css/style.css?0"
+ rel="stylesheet" type="text/css">
+ <link rel="shortcut icon" href="/images/icon.ico" />
+</head>
+<body>
+<table width="100%" border="0">
+<tbody>
+<tr>
+<td width = "33%"></td>
+<td width = "840px">
+<div class="main">
+<div class="header">
+<div class="logo">
+<table border="0" cellpadding="0" cellspacing="0" width="840">
+  <tbody>
+    <tr>
+      <td align="right"><b>Закрытое акционерное <br>общество "Петроклима"<br>Тел. +7 (812) 331-59-18</b></td>
+      <td width="140"><a href="/"><img src="/images/logo.jpg" border="0" height="165" width="140"></a></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<div class="menu">
+<div><a href="/">главная</a>
+| <a href="/okompanii.php">о
+компании</a>
+| <a href="/products.php">продукция</a>
+| <a href="contacts.php">контакты</a></div>
+</div>
+</div>
+<div class="content">
+<p align="center"><strong>Установка климатическая УК ПВ исп.02А</strong></p>
+<div class="normtext">
+  <!-------------------------------------------------------------------->
+  <br>
+  <p style="text-indent:30px">Установка климатическая УК ПВ исп.02А ТУ 4862-002-85583090-2009 предназначена для поддержания заданных параметров микроклимата в помещениях пассажирских вагонов железнодорожного транспорта, обеспечивая необходимые для этого вентиляцию, нагрев или охлаждение подаваемого в вагон воздуха.</p>
+  <br><p style="text-indent:30px">Электропитание установки осуществляется от автономных источников энергоснабжения вагона:</p>
+  <p style="text-indent:60px">- от подвагонного генератора при движении вагона со скоростью больше 40 км/ч;</p>
+  <p style="text-indent:60px">- от аккумуляторной батареи вагона или внешней сети во время стоянки и от трехканального преобразователя, питающегося от указанных источников.</p>
+  <p><br></p>
+  <p align="center"><strong>Основные параметры и характеристики</strong></p>
+  <p><br>
+  </p>
+<table cellspacing="0" cellpadding="5px" border="1"  width="780">
+  <tr>
+    <td width="580" valign="top"><p align="center"><strong>Наименование параметра</strong></p></td>
+    <td width="200" valign="top"><p align="center"><strong>Значение</strong></p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top"><p>1.    Холодопроизводительность, кВт*</p></td>
+    <td width="200" valign="top"><p align="center">28 ± 2</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top">2.    Максимальный расход приточного воздуха, м3/ч, в том числе:<br>
+      - расход наружного воздуха <br>
+      - расход рециркуляционного воздуха</td>
+    <td width="200" valign="top" align="center">4000 ± 400<br>1400 ± 140<br>2600 ± 260</td>
+  </tr>
+  <tr>
+    <td width="580" valign="top"><p>3.    Регулирование расхода приточного воздуха, м3/ч<br>
+    <td width="200" valign="top"><p align="center">1 ступень - (4000 ± 400)<br>2 ступень - (2500 ± 250)</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top"><p>4.    Избыточное давление воздуха на выходе из установки, Па, не менее</td>
+    <td width="200" valign="top"><p align="center">300</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top">5.    Максимальная потребляемая мощность в режиме «Охлаждение», кВт, не более<br></td>
+    <td width="200" valign="top"><p align="center">17,2</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top">6.    Теплопроизводительность электрокалориферов, кВт</td>
+    <td width="200" valign="top"><p align="center">6 ± 10 % (2 х 3)</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top"><p>7.    Теплопроизводительность жидкостных калориферов, кВт, не менее**</p></td>
+    <td width="200" valign="top"><p align="center">20</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top"><p>8.    Масса УК ПВ в заправленном состоянии, кг, не более</p></td>
+    <td width="200" valign="top"><p align="center">500</p></td>
+  </tr>
+  <tr>
+    <td width="580" valign="top" colspan="2"><p>* При расчетных параметрах:<br>
+- температуре наружного воздуха (40 ± 2) °С при относительной влажности (30 ± 3) %;<br>
+- температуре наружного воздуха (32 ± 2) °С при относительной влажности (70 ± 3) % и указанном расходе приточного воздуха.</p>
+    <p>
+       ** При расчетных параметрах: температуре теплоносителя не менее 90 °С, расходе теплоносителя не более 1,2 м3/ч, расходе воздуха через калориферы (2500 ± 250) м3/ч.
+    </p>
+    </td>
+  </tr>
+</table>
+<!-------------------------------------------------------------------->
+</div>
+</div>
+<div class="isoline"></div>
+</div>
+<td width = "33%"></td>
+</tr></tbody></table>
+</body>
+</html>
+
